94195ed491b70b79b9736ac23e9c9ed344969aec
Paul Anderson <anderson.paul.martin@gmail.com>
2013-06-07 01:43:41 -0400

    problem 3

--- problem_3.rb
+++ problem_3.rb
@@ -1,18 +1,21 @@
-limit = 1..600851475143
-numbers = 2..99
+#600851475143
+limit = Math.sqrt(600851475143)
+numbers = 2..limit
 
-limit.each do |num|
-	factors = []
-	numbers.each do |num2|
-		factors << num if num % num2 == 0
+factors = []
+numbers.each do |num|
+		factors << num if 600851475143 % num == 0
 	end
-	prime_factors = []
-	prime_factors << num if factors.length == 0
+
+#factors.each {|f| puts f}
+
+prime_factors = []
+factors.each do |num|
+	factor_factors = []
+	numbers.each do |num2|
+		factor_factors << num2 if num % num2 == 0 && !factor_factors.include?(num2) && num != num2
+		end
+	prime_factors << num if factor_factors.count == 0
 end
 
-prime_factors_2 = []
-prime_factors.each do |num|
-	prime_factors_2 << num if 600851475143 % num == 0
-end
-
-puts prime_factors_2.max
+puts prime_factors.max
